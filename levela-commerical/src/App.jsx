cc50b04c3df5af7cbae050ac0492c62d207bad14
Prabhjot Sodhi <sodhiprabhjot23@gmail.com>
2025-12-14 18:32:32 +1300

    Use home page components in app

--- levela-commerical/src/App.jsx
+++ levela-commerical/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Navbar1 } from "../components/Navbar1";
+import { Header98 } from "../components/Header98";
+import { Logo1 } from "../components/Logo1";
+import { Layout423 } from "../components/Layout423";
+import { Layout25 } from "../components/Layout25";
+import { Timeline17 } from "../components/Timeline17";
+import { Portfolio13 } from "../components/Portfolio13";
+import { Testimonial18 } from "../components/Testimonial18";
+import { Faq5 } from "../components/Faq5";
+import { Contact5 } from "../components/Contact5";
+import { Footer3 } from "../components/Footer3";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Navbar1 />
+      <Header98 />
+      <Logo1 />
+      <Layout423 />
+      <Layout25 />
+      <Timeline17 />
+      <Portfolio13 />
+      <Testimonial18 />
+      <Faq5 />
+      <Contact5 />
+      <Footer3 />
+    </div>
+  );
 }
 
-export default App
+export default App;
